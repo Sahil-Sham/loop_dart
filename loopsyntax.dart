b43be3476b093b274bc811a51589b7f82f7d60aa
Sahil-Sham <sahilsham173@gmail.com>
2024-02-02 13:58:32 +0500

    first commit

--- loopsyntax.dart
+++ loopsyntax.dart
@@ -1,7 +1,15 @@
-void main(){
-  // for loop syntax in dart:
-  for (var i = 0; i < 10; i++) {
-    print(i);
+// void main(){
+//   // for loop syntax in dart:
+//   for (var i = 0; i < 10; i++) {
+//     print(i);
     
-  }
+//   }
+// }
+
+
+void main (){
+  int abc=0;
+  do {
+    print('object');
+  } while (abc>3);  
 }
